39782c6865ef57a14e05aa9e0918bc9d59b5c299
Dmitry Aharkov <daharkov@mobilunity.com>
2019-08-23 14:02:33 +0300

    Updates

---
+++ precache-manifest.33290a3f9733b641bea9ea4e7f916fcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d0e871edbbc7408f6a602eac5df235e",
+    "url": "/acrobatics/index.html"
+  },
+  {
+    "revision": "17ba6b53e67faaa4f980",
+    "url": "/acrobatics/static/css/main.5d55456f.chunk.css"
+  },
+  {
+    "revision": "027843004dd69be3e624",
+    "url": "/acrobatics/static/js/2.2e1a35da.chunk.js"
+  },
+  {
+    "revision": "17ba6b53e67faaa4f980",
+    "url": "/acrobatics/static/js/main.d11cfe2c.chunk.js"
+  },
+  {
+    "revision": "caf1d6c123ec14c61dfd",
+    "url": "/acrobatics/static/js/runtime~main.0cea684a.js"
+  },
+  {
+    "revision": "bab218c537ec561e12e200267b473874",
+    "url": "/acrobatics/static/media/background.bab218c5.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e3148a26d8038a32a4a40a2c1608aad4.js"
+  "/acrobatics/precache-manifest.33290a3f9733b641bea9ea4e7f916fcb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/acrobatics/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
